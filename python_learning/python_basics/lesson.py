3fc72839c625a078211101763b306ac01a5aa40a
Hernandez <dhernandez209@cps.k12.il.us>
2024-09-12 14:45:10 -0500

    first commit

---
+++ python_learning/python_basics/lesson.py
@@ -0,0 +1,29 @@
+#mkdir means make directory
+#cd means change directory
+#ls means list directory
+#pwd means print wprking directory
+#touch means create a new file 
+#rm means remove a file 
+#pound sign means comment
+# print("Hello, World!")
+# print("Diego Hernandez")
+# print("November 9th \t 25")
+# press control ? to comment out multiple lines 
+#\t means tab
+name = input("what is your name? ")
+print("Hello, " + name)
+# ask for age
+#print out age
+# ask for their place of birth
+#ask for their address 
+# print out address
+# ask what school they went to 
+# print out school
+age  = input("what is your age? ")
+print("wait you're, " + age )
+place_of_birth = input("what is you're place of birth?")
+print("You're place of birth is," + place_of_birth)
+address = input("where do you live?")
+print("you live at?"+ address)
+school = input("what school do you go to")
+print("you go to?" + school)
